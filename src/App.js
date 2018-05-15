3cd3a7fa0b076d67180a8a2bcde93540d622485a
Edu Iglesias <eiglesiash@gmail.com>
2018-05-15 17:12:40 +0200

    Ready for sections

--- src/App.js
+++ src/App.js
@@ -39,72 +39,14 @@ class Text extends Component {
   }
 }
 
-class Title extends Component {
-  render(){
-    return (
-        <div>
-          <h2>Title Component</h2>
-          <h3>{this.props.text}</h3>
-        </div>
-        )
-  }
-}
-
-Title.defaultProps = {
-  text: "Title for default"
-}
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = { counter: this.props.counterInitial };
-    setInterval(() => {
-      this.setState({counter: this.state.counter +1})
-    }, 1000)
-  }
-  render(){
-    console.log('Counter Number Render()');
-    return (
-        <div>
-          <h2>Counter Component</h2>
-          <CounterNumber number={this.state.counter} />
-        </div>
-    )
-  }
-}
-
-
-Counter.defaultProps = {
-  counterInitial: 0
-}
-
-class CounterNumber extends Component {
-  render () {
-    return (
-        <div>
-          <h2>Component CounterNumber</h2>
-          <span>{ this.props.number }</span>
-        </div>
-    )
-  }
-}
 class App extends Component {
   render() {
     return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo"/>
-            <Hello title={"Hello from props"}/>
+            <Hello title={"Ready for section??"}/>
           </header>
-          <Text
-              arrayOfNumbers={[1, 3, 6]}
-              objectWithInfo={{key: 'Uno', key2: 'Dos'}}
-              text={"This is my component text"}
-              multiply={(number) => number * 8}
-              title={<h1>This is the title</h1>}
-          />
-          <Title text={"Title not default"}/>
-          <Counter counterInitial={500}/>
         </div>
     );
   }
