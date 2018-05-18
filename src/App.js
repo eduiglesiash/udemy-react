bdc5b5eea74d2c029107aa136a6ae8fc65c91648
Edu Iglesias <eiglesiash@gmail.com>
2018-05-18 22:45:45 +0200

    Finish section9

--- src/App.js
+++ src/App.js
@@ -17,44 +17,44 @@ class Hello extends Component {
   }
 }
 
-class Button extends Component {
-  render() {
-    return (
-        <button
-            style={{
-              borderColor: this.props.borderColor,
-              display: 'block'
-            }}
-        > {this.props.label}
-        </button>
-    )
-  }
-}
-Button.defaultProps = {
-  borderColor: '#09f'
-}
-
-class ButtonDanger extends Component {
-  render(){
-    return <Button
-        borderColor={"red"}
-        label={this.props.label}
-    />
-  }
-}
-
-class ButtonWithLegend extends Component {
-  render(){
-    return(
-        <div>
-          <Button
-          label={this.props.label}
-          borderColor={this.props.borderColor}/>
-          <small>{this.props.legend}</small>
-        </div>
-    )
-  }
-}
+// class Button extends Component {
+//   render() {
+//     return (
+//         <button
+//             style={{
+//               borderColor: this.props.borderColor,
+//               display: 'block'
+//             }}
+//         > {this.props.label}
+//         </button>
+//     )
+//   }
+// }
+// Button.defaultProps = {
+//   borderColor: '#09f'
+// }
+//
+// class ButtonDanger extends Component {
+//   render(){
+//     return <Button
+//         borderColor={"red"}
+//         label={this.props.label}
+//     />
+//   }
+// }
+//
+// class ButtonWithLegend extends Component {
+//   render(){
+//     return(
+//         <div>
+//           <Button
+//           label={this.props.label}
+//           borderColor={this.props.borderColor}/>
+//           <small>{this.props.legend}</small>
+//         </div>
+//     )
+//   }
+// }
 
 class App extends Component {
   render() {
@@ -64,17 +64,8 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo"/>
             <Hello title={"Ready for section 9"}/>
           </header>
-          <h2>Composición vs Herencia</h2>
-          <Button label='Click Here Composicion'/>
-          <ButtonDanger label='Warning Composicion'/>
+          <h2>Stateless Components</h2>
 
-          <br />
-
-          <ButtonWithLegend
-              label={'Button Whith Legend Composicion'}
-              legend={'Clicka el boton para hacer algo'}
-
-          />
         </div>
     );
   }
